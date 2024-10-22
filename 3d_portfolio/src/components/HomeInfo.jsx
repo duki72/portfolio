b01fe760563173475f65bf3b73a2eadb0829cd2d
skadu07 <duskadamjanovic493@gmail.com>
2024-10-22 22:52:15 +0200

    first commit

---
+++ 3d_portfolio/src/components/HomeInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomeInfo = () => {
+    return (
+        <div className="home-info">
+            <h1>Welcome to My Portfolio</h1>
+            <p>
+                Hi, I'm [Your Name], a passionate developer with expertise in creating
+                stunning web applications. Explore my projects and get to know more about my work.
+            </p>
+        </div>
+    );
+};
+
+export default HomeInfo;
---
+++ 3d_portfolio/src/models/Sky.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+
+import skyScene from "../assets/3d/sky.glb";
+
+
+export function Sky ({isRotating}) {
+
+    const sky = useGLTF(skyScene);
+    const skyRef = useRef();
+
+
+    
+  // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
+  // It ensures smooth animations by making the rotation frame rate-independent.
+  // 'delta' represents the time in seconds since the last frame.
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyRef.current.rotation.y += 0.25 * delta; // Adjust the rotation speed as needed
+    }
+  });
+  
+    return (
+        <mesh ref={skyRef}>
+          // use the primitive element when you want to directly embed a complex 3D
+          model or scene
+          <primitive object={sky.scene} />
+        </mesh>
+      );
+}
+
+export default Sky
