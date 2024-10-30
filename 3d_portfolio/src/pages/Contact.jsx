4eb3e6bae37b948e211ff740cb0b81dfb9363b1c
skadu07 <duskadamjanovic493@gmail.com>
2024-10-30 16:59:45 +0100

    contract

--- 3d_portfolio/src/pages/Contact.jsx
+++ 3d_portfolio/src/pages/Contact.jsx
@@ -1,8 +1,152 @@
 import React from 'react'
+import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useRef, useState } from "react";
+import {Loader} from "../components";
+import { Fox } from "../models";
 
 const Contact = () => {
+
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+  const [loading, setLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleFocus = () => setCurrentAnimation("walk");
+
+
+  const handleBlur = () => setCurrentAnimation("idle");
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+    console.log(form)
+    setCurrentAnimation("hit");
+    emailjs.send(import.meta.env.VITE_SERVICE_ID, 
+      import.meta.env.VITE_TEMPLATE_ID, {from_name: form.name,
+        to_name: "Duska Damjanovic",
+        from_email: form.email,
+        to_email: "duskadamjanovic493@gmail.com",
+        message: form.message,},
+       import.meta.env.VITE_PUBLIC_KEY)
+
+       .then(
+        () => {
+          setLoading(false);
+          alert("Your message has been sent. Thank you!");
+          setForm({ name: "", email: "", message: "" });
+          setCurrentAnimation("idle");
+        }
+      ).catch(
+        () => {
+          setLoading(false);
+          alert("Something went wrong. Please try again.")
+          setCurrentAnimation("idle");
+        }
+      )
+  }
+
+
+
   return (
-    <div>Contact</div>
+   <section className=' relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+
+        <h1 className='head-text'>Get in touch</h1>
+
+        <form className='w-full flex flex-col gap-7 mt-14'>
+        <label className='text-black-500 font-semibold'>
+            Name
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder='John'
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+              type='email'
+              name='email'
+              className='input'
+              placeholder='John@gmail.com'
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className='text-black-500 font-semibold'>
+            Your Message
+            <textarea
+              name='message'
+              rows='4'
+              className='textarea'
+              placeholder='Write your thoughts here...'
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type='submit'
+            disabled={loading}
+            className='btn'
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {loading ? "Sending..." : "Submit"}
+          </button>
+        </form>
+      </div>
+
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}
+        >
+          <directionalLight position={[0, 0, 1]} intensity={2.5} />
+          <ambientLight intensity={1} />
+          <pointLight position={[5, 10, 0]} intensity={2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.629, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
   )
 }
 
--- 3d_portfolio/src/pages/Projects.jsx
+++ 3d_portfolio/src/pages/Projects.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section className='max-container'>
+      <h1 className='head-text'>
+        My{" "}
+        <span className='blue-gradient_text font-semibold drop-shadow'>
+          {" "}
+          Projects
+        </span>{" "}
+        👋
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          Software Engineer based in Croatia, specializing in technical
+          education through hands-on learning and building applications.
+        </p>
+      </div>
+
+      <div className='mt-10 grid grid-cols-1 md:grid-cols-2 gap-10'>
+        
+
+      </div>
+      </section>
   )
 }
 
